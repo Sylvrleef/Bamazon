6d9be28c4eaf4e64e9654409edc868bf1f571813
Sylvrleef <sylvrleef@gmail.com>
2017-12-14 18:35:41 -0600

    finish part 1 homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,39 +15,65 @@ connection.connect(function(err) {
     if (err) throw err;
 
     for (var i = 0; i < results.length; i++) {
-      console.log("Product ID: ", results[i].item_id, "Product: ", results[i].product_name, "Price: ", results[i].price);
+      console.log("Product ID:", results[i].item_id, "\nProduct:", results[i].product_name, "\nPrice: $" + results[i].price + ".00\n\n");
     }
-itemQuestion();
+    itemQuestion();
 
   });
 });
 
-function itemQuestion (){
-  inquirer.prompt([
-    {
-      name: "id",
-      message: "What item would you like to buy? Please enter the item ID."
-    }, {
-      name: "quantity",
-      message: "How many do you want?"
-    },
-  ]).then(function(answers) {
+function itemQuestion() {
+  inquirer.prompt([{
+    name: "id",
+    message: "What item would you like to buy? Please enter the item ID."
+  }, {
+    name: "quantity",
+    message: "How many do you want?"
+  }, ]).then(function(answers) {
 
-    console.log(answers);
-
-    connection.query("select stock_quantity from products where item_id = " + answers.id, function(err, results) {
-if (answers.quantity > results) {
-  console.log("I'm sorry there is insufficiant quantity to purchase " + answers.quantity + ".");
-} else {
-  console.log("Your total is " + orderTtotal + ".");
-  removeQuantity()
-}
+    connection.query("select * from products where item_id = " + answers.id, function(err, results) {
+      if (answers.quantity > results[0].stock_quantity) {
+        console.log("I'm sorry there is insufficiant quantity to purchase " + answers.quantity + " of " + results[0].product_name + ".");
+        correctQuantity(answers.id, results[0].product_name);
+      } else {
+        removeQuantity(answers.quantity, answers.id, results[0].stock_quantity, results[0].price);
+      }
     });
-
   });
 
-}
+};
 
-function removeQuantity(answers.quantity) {
-  
-}
+function removeQuantity(quantity, id, num, price) {
+  var orderTotal = quantity * price;
+  console.log("Your total is $" + orderTotal + ".00.");
+  var newQuantity = num - quantity;
+  var sql = "UPDATE products SET stock_quantity = " + newQuantity + " WHERE item_id = " + id + ";";
+  var updatedQuantity = "SELECT stock_quantity FROM products;";
+    connection.query(sql, function (err, results) {
+      if (err) throw err;
+      connection.query(updatedQuantity, function(err,results) {
+        if(err) throw err;
+      })
+      connection.end();
+
+    });
+
+};
+
+function correctQuantity(id, product){
+  inquirer.prompt([{
+    name: "correctQuantity",
+    message: "How many " + product + " do you want?"
+  }, ]).then(function(answers) {
+
+       connection.query("select * from products where item_id = " + id, function(err, results) {
+        if (answers.correctQuantity > results[0].stock_quantity) {
+          console.log("I'm sorry there is insufficiant quantity to purchase " + answers.quantity + " of " + results[0].product_name + ".");
+          correctQuantity(id);
+        } else {
+          var orderTotal = answers.correctQuantity * results[0].price;
+          removeQuantity(answers.correctQuantity, id, results[0].stock_quantity, results[0].price);
+        };
+      });
+    });
+};
